2ae2b7a565e2fb2736945d5bd5ee25cb30c5d961
Anshul889 <anshulmanaktala@gmail.com>
2020-10-07 12:50:58 +0530

    remove firebase config

---
+++ src/app/config/firebase.js
@@ -0,0 +1,31 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/database";
+import "firebase/auth";
+import "firebase/storage";
+import "firebase/functions";
+import 'firebase/analytics';
+import 'firebase/messaging';
+import 'firebase/performance'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDszVO3XwuuFKoEifj2RBWfeEcb1lZHe2I",
+  authDomain: "luxxette.firebaseapp.com",
+  databaseURL: "https://luxxette.firebaseio.com",
+  projectId: "luxxette",
+  storageBucket: "luxxette.appspot.com",
+  messagingSenderId: "767219364985",
+  appId: "1:767219364985:web:54a00ebc069fa7473890a8",
+  measurementId: "G-CQWW0N8CD6"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
+firebase.functions();
+firebase.analytics();
+firebase.messaging();
+firebase.performance();
+firebase.firestore().enablePersistence();
+
+export default firebase;
